1386730839c3278ddc3f96065d8236bb09942166
Alex Lee <Alex Lee>
2017-03-12 12:23:54 -0600

    no compile errors

--- student-distrib/i8259.h
+++ student-distrib/i8259.h
@@ -15,12 +15,13 @@
 /* Initialization control words to init each PIC.
  * See the Intel manuals for details on the meaning
  * of each word */
-#define ICW1    0x11
+#define ICW1    	  0x11
 #define ICW2_MASTER   0x20
 #define ICW2_SLAVE    0x28
 #define ICW3_MASTER   0x04
 #define ICW3_SLAVE    0x02
-#define ICW4          0x01
+#define ICW4_MASTER	  0x05
+#define ICW4_SLAVE    0x01
 
 /* End-of-interrupt byte.  This gets OR'd with
  * the interrupt number and sent out to the PIC
@@ -28,7 +29,7 @@
 #define EOI             0x60
 
 /* Slave line constant */
-#define SLAVE_IRQ	2
+#define SLAVE_IRQ_LINE	2
 
 /* Externally-visible functions */
 
